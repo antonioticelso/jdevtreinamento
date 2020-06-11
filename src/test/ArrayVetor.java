ee52fbb59ec856d5a3a4a8e923d5aa1ef29dbe6d
C1314151 Antonio Celso Prado De Magalhaes <antonio.ti.celso@gmail.com>
2020-06-11 17:41:50 -0300

    atulizacao de projeto 11/06/2020

---
+++ src/test/ArrayVetor.java
@@ -0,0 +1,25 @@
+package test;
+
+import javax.swing.*;
+
+public class ArrayVetor {
+    public static void main(String[] args) {
+        String[] valores = new String[4];
+        valores[0] = "antonio";
+        valores[1] = "java";
+        valores[2] = "full stack";
+        valores[3] = "eu";
+
+//        Primeira forma de trabalhar com o Array
+//        String posicoes = JOptionPane.showInputDialog("Quantas notas seram inseridas? ");
+//        double notas[] = new double[Integer.parseInt(posicoes)];
+//
+//        for (int pos = 0; pos < notas.length; pos++) {
+//            String valor = JOptionPane.showInputDialog("Informe o valor da " + (pos + 1) + " : ");
+//            notas[pos] = Double.valueOf(valor);
+//        }
+//        for (int pos = 0; pos < notas.length; pos++) {
+//            System.out.println("Nota: " + (pos + 1) + " eh: " + notas[pos]);
+//        }
+    }
+}
